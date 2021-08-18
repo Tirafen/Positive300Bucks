438b9eb3f05a3213f09000549a48161e727d82b3
tirafen <tirafen@gmail.com>
2021-08-18 15:57:32 +0700

    декор

--- main.py
+++ main.py
@@ -45,8 +45,7 @@ async def read_user(user_id: UUID, db: Session = Depends(get_db)):
 
 
 @app.post("/users/{user_id}/orders/", response_model=schemas.Order)  # Создание нового заказа
-async def create_order(
-    user_id: UUID, item: schemas.OrderCreate, db: Session = Depends(get_db)):
+async def create_order(user_id: UUID, item: schemas.OrderCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
