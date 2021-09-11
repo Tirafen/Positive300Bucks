4c1cc3ddc6e0cb3f378ab5d163b56151cc708ca5
tirafen <tirafen@gmail.com>
2021-09-11 13:22:48 +0700

    Фикс адресов доступа к очереди, убран result backend нахрен

--- celery_app/celeryconfig.py
+++ celery_app/celeryconfig.py
@@ -1,6 +1,6 @@
-broker_url = 'amqp://guest:guest@localhost:5672'
-result_backend = 'db+postgresql://postgres:Q1w2e3r4t5@127.0.0.1/test_db'
-backend_url = 'amqp://guest:guest@localhost:5672'
+broker_url = 'amqp://guest:guest@rabbitmq:5672'
+#result_backend = 'db+postgresql://postgres:Q1w2e3r4t5@0.0.0.0:5432/test_db'
+backend_url = 'amqp://guest:guest@rabbitmq:5672'
 
 celery_track_started = True
 celery_concurrency = 2
