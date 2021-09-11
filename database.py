799937f96e9a717c50d61b394499a3045caa4c24
tirafen <tirafen@gmail.com>
2021-09-11 13:22:12 +0700

    Фикс адреса доступа к БД

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Q1w2e3r4t5@127.0.0.1/test_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Q1w2e3r4t5@db/test_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
